dfb5a9a5aef3cd3f6e3650666abf0592b5de270a
GUSTAVO LEANDRO MOLINA <1051392421037@fatec.corp>
2026-06-02 10:23:07 -0300

    aula 03

---
+++ Tra-CI-ID-main/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Gerenciamento de Biblioteca"
+
+@app.route("/sobre")
+def sobre():
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
+
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
+
+@app.route("/cadastro-livro")
+def cadastro_livro():
+    return "Formulário de cadastro de livros"
+
+@app.route("/autores")
+def autores():
+    return "Lista de autores cadastrados"
+
+@app.route("/contato")
+def contato():
+    return "Página de contato do sistema"
+
+@app.route("/status")
+def status():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
