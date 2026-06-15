6eb81bd3184530c62a132f7cee47e102c9390cf8
João Emanoel <emanuel23pagani76@gmail.com>
2026-06-15 19:24:32 -0300

    chore: prepara aplicação para deploy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, os
+
 
 app = Flask(__name__)
 
@@ -32,4 +33,5 @@ def status():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
